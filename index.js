c3ce40a29b3eb6b2530ba8dd20d89b2dd37fc4ec
Ambar Khare <ambar.khare24@gmail.com>
2022-07-31 12:10:03 -0700

    commit tree c8e579ffb34710093118bae1307a8b95f8f13641

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let birthdayCards = []
+    for (i = 0; i < array.length; i++) {
+        birthdayCards.push(`Thank you, ${array[i]} for the wonderful ${event} gift!`)
+    }
+    return birthdayCards
+}
+
+function countDown(i) {
+  while (i > 0) {
+    console.log(i);
+    i -= 1;
+  }
+  return i;
+}
